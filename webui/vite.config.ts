3f4dbf86a97ed4ca938abc3a8d9c3fd9fac4b596
KOWX712 <leecc0503@gmail.com>
2026-07-09 22:58:46 +0800

    chore: re-create webui

---
+++ webui/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  build: {
+    outDir: '../module/webroot',
+    chunkSizeWarningLimit: 1000,
+  },
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
